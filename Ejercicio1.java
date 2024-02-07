d0d7fb32d0ffb4900665f40f902f93fd664a003c
sergiioo101 <smartros@myuax.com>
2024-02-07 16:37:49 +0100

    ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,16 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int diasEnUnAnio = 365;
+        int horasEnUnDia = 24;
+        int minutosEnUnaHora = 60;
+        int segundosEnUnMinuto = 60;
+
+        int segundosEnUnAnio = calcularSegundosEnUnAnio(diasEnUnAnio, horasEnUnDia, minutosEnUnaHora, segundosEnUnMinuto);
+        System.out.println("El número de segundos en un año es: " + segundosEnUnAnio);
+    }
+
+    public static int calcularSegundosEnUnAnio(int dias, int horas, int minutos, int segundos) {
+        int segundosEnUnDia = horas * minutos * segundos;
+        return dias * segundosEnUnDia;
+ }
+}
