fb57fb4f7e55d7233b536ed0ec2275371585cba5
sergiioo101 <smartros@myuax.com>
2024-02-07 16:56:06 +0100

    ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,12 @@
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        char maxChar = Character.MAX_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+
+        System.out.println("El mayor valor para un char es: " + (int)maxChar);
+        System.out.println("El mayor valor para un short es: " + maxShort);
+        System.out.println("El mayor valor para un int es: " + maxInt);
+    }
+}
